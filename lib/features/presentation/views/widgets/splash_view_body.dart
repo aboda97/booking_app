2577d1f1bdc0fa97bde3461b63ba7a00afffb560
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2023-10-31 14:07:54 +0200

    add text to splash view and update theme

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -2,7 +2,7 @@ import 'package:bookly_app/core/utils/asstes_variables.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
-   const SplashViewBody({super.key});
+  const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +11,10 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsVariables.bookingLogo),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
