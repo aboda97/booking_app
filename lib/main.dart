a8f2322f2d4f99d56cccceacf3db3a108fe48aa0
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2023-10-31 12:31:00 +0200

    add structure of MVVM and splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BookingApp());
@@ -9,9 +11,9 @@ class BookingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: SplashView(),
     );
   }
 }
