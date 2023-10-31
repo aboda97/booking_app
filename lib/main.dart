ef80db8187b47d169c97f12880ef996939bf8a7b
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2023-10-31 11:40:37 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BookingApp());
+}
+
+class BookingApp extends StatelessWidget {
+  const BookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
