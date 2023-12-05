22026e5bb78506f8034d0e952699f914ffcb6f86
Ahmed Abdelhamid <54990238+aboda97@users.noreply.github.com>
2023-12-05 14:21:31 +0200

    add button to header list view image

--- lib/features/home/presentation/views/widgets/header_list_view_builder.dart
+++ lib/features/home/presentation/views/widgets/header_list_view_builder.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/widgets/custom_circluar_loading_indicator_widget.dart';
 import 'package:bookly_app/core/widgets/custom_error_widget.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/core/widgets/custom_image_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class HeaderListViewBuilder extends StatelessWidget {
   const HeaderListViewBuilder({super.key});
@@ -20,10 +22,18 @@ class HeaderListViewBuilder extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 itemCount: state.featuredBooksData.length,
                 itemBuilder: (context, index) {
-                  return CustomImageItem(
-                    imgurl: state.featuredBooksData[index].volumeInfo.imageLinks
-                            ?.thumbnail ??
-                        '',
+                  return GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(
+                        AppRouter.kBooksDetailsView,
+                        extra: state.featuredBooksData[index],
+                      );
+                    },
+                    child: CustomImageItem(
+                      imgurl: state.featuredBooksData[index].volumeInfo
+                              .imageLinks?.thumbnail ??
+                          '',
+                    ),
                   );
                 }),
           );
